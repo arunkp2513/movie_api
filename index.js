c00ee4e49f9f90fdeeda7bf8974f6e2d7f64a8ce
arunkp2513 <99520584+arunkp2513@users.noreply.github.com>
2022-05-05 23:48:11 -0700

    Removed authentication for movies

--- index.js
+++ index.js
@@ -39,15 +39,25 @@ app.get('/', (req, res) => {
 });
 
 //Return all the movie
-app.get('/movies', passport.authenticate('jwt' , {session: false}) , (req,res) =>{
+//app.get('/movies', passport.authenticate('jwt' , {session: false}) , (req,res) =>{
+ // Movies.find()
+ // .then((movies) =>{
+ //   res.status(201).json(movies);
+ // })
+  //.catch((err) =>{
+   // console.error(err);
+   // res.status(500).send('Error : ' + err);
+  //});
+//});
+app.get("/movies", function (req, res) {
   Movies.find()
-  .then((movies) =>{
-    res.status(201).json(movies);
-  })
-  .catch((err) =>{
-    console.error(err);
-    res.status(500).send('Error : ' + err);
-  });
+    .then(function (movies) {
+      res.status(201).json(movies);
+    })
+    .catch(function (error) {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
 });
 
 //GET all users
